912d39dd6656c220282f4f91e5b61f85d65d7dbb
usoken <kaimongchalmer@gmail.com>
2017-05-11 16:21:48 -0400

    Update app.js

--- test/app.js
+++ test/app.js
@@ -6,11 +6,11 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
     if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-	    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-	    var password = process.env.MLAB_PASSWORD_WEBDEV;
-	    connectionString = 'mongodb://' + username + ':' + password;
-	    connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
-}
+        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+        var password = process.env.MLAB_PASSWORD_WEBDEV;
+        connectionString = 'mongodb://' + username + ':' + password;
+        connectionString += '@ds133388.mlab.com:33388/heroku_pznwddsl'; // user yours
+    }
 
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
