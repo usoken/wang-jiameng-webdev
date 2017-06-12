2f62b8466da294f58314a60e7e4b2d4fa6e5af00
WangJiameng <Chalmer@Chalmerdiaobudiao.local>
2017-06-11 22:29:25 -0400

    add mlab

--- assignment/app.js
+++ assignment/app.js
@@ -11,6 +11,13 @@ module.exports = function(app) {
     var mongoose = require('mongoose');
     mongoose.Promise = require('q').Promise;
 
+    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+        var password = process.env.MLAB_PASSWORD_WEBDEV;
+        connectionString = 'mongodb://' + username + ':' + password;
+        connectionString += '@ds133388.mlab.com:33388/heroku_pznwddsl'; // user yours
+    }
+
     try {
         mongoose.connect('mongodb://127.0.0.1/test'); //- starting a db connection
     }catch(err) {
